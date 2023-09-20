a9089e062ce653ee1cbafcc540bfa71aa889daf7
orji christopher <chigaemezuorji18@gmail.com>
2023-09-20 13:11:43 +0100

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const Main = () => {
+
+    const  [movie,  setMovie] =  useState([])
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Main
